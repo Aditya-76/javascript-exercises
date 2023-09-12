8999db3f856f7b7f88839ec449b8910033889e38
Aditya Shekhar <aditya7682@gmail.com>
2023-09-12 19:28:34 +0530

    Solved problems 01_helloWorld, 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(str,n) {
+    let res = "";
+    for (let i=0;i<n;i++){
+        res += str;
+    }
+    return res;
 };
 
 // Do not edit below this line
