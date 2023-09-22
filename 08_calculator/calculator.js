5104b51e1f7eb6c348a4a138dcf42e6e18177d3d
Aditya Shekhar <aditya7682@gmail.com>
2023-09-22 15:48:35 +0530

    Solve 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+	let res = 0;
+  nums.forEach(s => res += s);
+  return res;
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+ let res = 1;
+  nums.forEach(m => res*=m);
+  return res;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if (n<=1) return 1;
+  let res = 1;
+  for (let i=2;i<=n;i++) res *= i;
+  return res;
 };
 
 // Do not edit below this line
