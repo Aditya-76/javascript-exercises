4afa475e23a000d5e0f232a75436f64afb64c096
Aditya Shekhar <aditya7682@gmail.com>
2023-09-22 16:07:58 +0530

    Solve 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (s) {
+    s = s.toLowerCase().replace(/[^a-z0-9]/g, "");
+    return s==s.split('').reverse().join('');
 };
 
 // Do not edit below this line
