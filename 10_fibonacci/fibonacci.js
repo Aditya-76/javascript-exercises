c0b783df6163681e3edafcd51daf8f8cd98ec287
Aditya Shekhar <aditya7682@gmail.com>
2023-09-22 16:15:29 +0530

    Solve 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    n = parseInt(n);
+    if (n<=0) return 'OOPS';
+    let fibo = [1,1];
+    for (let i=2; i<=n; i++) fibo.push(fibo[i-1]+fibo[i-2]);
+    // console.log(fibo);
+    return fibo[n-1];
 };
 
 // Do not edit below this line
